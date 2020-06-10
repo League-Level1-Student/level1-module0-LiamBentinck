621f774f1592018f96739f61bff0abcfada3da0a
LiamBentinck <50850141+LiamBentinck@users.noreply.github.com>
2020-06-10 16:57:17 -0700

    ondrumkit

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -25,36 +25,47 @@ public class PhotoQuiz {
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
-		// “Copy Image Address”)
-
+		// â€œCopy Image Addressâ€�)
+		String imageURL = "https://blog.hubspot.com/hubfs/image8-2.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component image;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		image = createImage(imageURL);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String answer = JOptionPane.showInputDialog(null, "Do you like google?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if (answer.equalsIgnoreCase("Yes")) {
+			System.out.println("Correct");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
+		if (answer.equalsIgnoreCase("No")) {
+			System.out.println("Incorrect");
+		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(image);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		String imageURL2 = "https://s3-prod.adage.com/s3fs-public/styles/800x600/public/Microsoft2016011332.jpg";
+		Component image2;
+		image2 = createImage(imageURL2);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(image2);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String answer2 = JOptionPane.showInputDialog("Do you like Bing");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if (answer2.equalsIgnoreCase("Yes")) {
+			System.out.println("Correct");
+		}
+		if (answer2.equalsIgnoreCase("No")) {
+			System.out.println("Incorrect");
+		}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
